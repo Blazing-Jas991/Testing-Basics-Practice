bb7e8e31c1165d973ee3780a6d5663f005634896
Blazing-Jas991 <223303023+Blazing-Jas991@users.noreply.github.com>
2026-06-12 09:11:05 -0700

    add testing functions

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,41 +1,96 @@
 // Capitalize.js
 
-export function capitalize (letter) {
-    const word = letter.charAt(0).toUpperCase() + letter.slice(1).toLowerCase();
-    return word;
+export function capitalize(letter) {
+  const word = letter.charAt(0).toUpperCase() + letter.slice(1).toLowerCase();
+  return word;
 }
 
 export function reverseString(str) {
-    // const letter = str.split("");
-    // const reversedLetter = letter.reverse();
-    // const joinedLetter = reversedLetter.join();
-    // return joinedLetter;
+  // Other ways to get the same result
 
-    // let newString = "";
-    // for(let i = str.length - 1; i >= 0; i--) {
-    //     newString += [i];
-    // }
+  // const letter = str.split("");
+  // const reversedLetter = letter.reverse();
+  // const joinedLetter = reversedLetter.join();
+  // return joinedLetter;
 
-    return str.split("").reverse().join("");
+  // let newString = "";
+  // for(let i = str.length - 1; i >= 0; i--) {
+  //     newString += [i];
+  // }
+
+  return str.split('').reverse().join('');
 }
 
 export function calculate(num1, operator, num2) {
-    let sum;
-    switch(operator) {
-        case '+':
-            sum = num1 + num2;
-            break;
-        case '*':
-            sum = num1 * num2;
-            break;
-        case '-':
-            sum = num1 -num2;
-            break;
-        case '/':
-            sum = num1 / num2;
-            break;
-        default:
-            sum = 'Invalid Operator';
-    }
-    return sum;
+  let sum;
+  switch (operator) {
+    case '+':
+      sum = num1 + num2;
+      break;
+    case '*':
+      sum = num1 * num2;
+      break;
+    case '-':
+      sum = num1 - num2;
+      break;
+    case '/':
+      sum = num1 / num2;
+      break;
+    default:
+      sum = 'Invalid Operator';
+  }
+  return sum;
 }
+
+export function caesarCipher(str, shift) {
+  // Normalize the shift key to stay within 0-25 range
+  const normalizedShift = ((shift % 26) + 26) % 26;
+
+  // Split string, map each character, and join them back
+  return str
+    .split('')
+    .map((char) => {
+      const code = char.charCodeAt(0);
+
+      // Handle Uppercase letters (ASCII 65 to 90)
+      if (code >= 65 && code <= 90) {
+        return String.fromCharCode(((code - 65 + normalizedShift) % 26) + 65);
+      }
+
+      // Handle Lowercase letters (ASCII 97 to 122)
+      if (code >= 97 && code <= 122) {
+        return String.fromCharCode(((code - 97 + normalizedShift) % 26) + 97);
+      }
+
+      // Pass spaces, numbers, and special characters through unmodified
+      return char;
+    })
+    .join('');
+}
+
+export function arrayAnalyzer(arr) {
+  const arrAverage = (arr) => arr.reduce((a, b) => a + b) / arr.length;
+
+  const arrayData = {
+    average: arrAverage(arr),
+    min: Math.min(...arr),
+    max: Math.max(...arr),
+    length: arr.length,
+  };
+
+  return arrayData;
+}
+
+// Functions to get the minimum and maximum number from an array
+
+// function arrayMin(arr) {
+//   return arr.reduce(function (p, v) {
+//     return p < v ? p : v;
+//   });
+// }
+
+// function arrayMax(arr) {
+//   return arr.reduce(function (p, v) {
+//     return p > v ? p : v;
+//   });
+// }
--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -1,18 +1,41 @@
 // Capitalize.test.js
 
-import { capitalize, reverseString, calculate } from "./capitalize.js";
+import {
+  capitalize,
+  reverseString,
+  calculate,
+  caesarCipher,
+  arrayAnalyzer,
+} from './capitalize.js';
 
 test('Turns the first letter of a word into a Capital letter', () => {
-    expect(capitalize('letter')).toBe('Letter');
+  expect(capitalize('letter')).toBe('Letter');
 });
 
 test('Reverses a string', () => {
-    expect(reverseString('jasper')).toBe('repsaj');
+  expect(reverseString('jasper')).toBe('repsaj');
 });
 
 test('Perform Basic calculations', () => {
-    expect(calculate(2, '+', 2)).toBe(4);
-    expect(calculate(2, '*', 3)).toBe(6);
-    expect(calculate(10, '-', 5)).toBe(5);
-    expect(calculate(50, '/', 5)).toBe(10);
+  expect(calculate(2, '+', 2)).toBe(4);
+  expect(calculate(2, '*', 3)).toBe(6);
+  expect(calculate(10, '-', 5)).toBe(5);
+  expect(calculate(50, '/', 5)).toBe(10);
+});
+
+test('Takes a word and a shift key and translates into caesar cipher', () => {
+  expect(caesarCipher('jasper', 3)).toBe('mdvshu');
+  expect(caesarCipher('jasper', 4)).toBe('newtiv');
+  expect(caesarCipher('jasper is the, king', 3)).toBe('mdvshu lv wkh, nlqj');
+});
+
+const newArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+test('Takes an array and returns an object with the min, max, average values and length of the array', () => {
+  expect(arrayAnalyzer(newArr)).toEqual({
+    average: 5,
+    length: 9,
+    max: 9,
+    min: 1,
+  });
 });
